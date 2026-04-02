675864315c23986d7091f8e66902bc90c35c1b9e
Ckott <diegoangeles130703@gmail.com>
2026-04-01 21:57:17 -0500

    feat: update title and enhance App component layout; remove unused App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { Button } from './components/ui/button'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import { Button } from "./components/ui/button";
+import { BotMessageSquare } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button>
-        Hello world
-      </Button>
+      <div className="text-primary font-extrabold flex gap-2 items-center p-2 justify-between border-b">
+        <a href="" className="flex items-center gap-2">
+          <BotMessageSquare /> Ali
+        </a>
+        <Button>Iniciar Session</Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
